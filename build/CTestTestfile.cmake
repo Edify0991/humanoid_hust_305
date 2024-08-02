fc028ae4b6df44b15b38a1e906fddcd093355847
Edify_Kd <439507039@qq.com>
2024-08-02 14:56:59 +0800

    新加了IMU相关内容

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lhl/project/humanoid_hust_305/src
-# Build directory: /home/lhl/project/humanoid_hust_305/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("line_motor_comm_pkg")
